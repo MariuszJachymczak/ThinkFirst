93ff1cfcb162e33a55db4da8e85d95de7c36a053
mariusz.jachymczak <mariusz.jachymczak@bpower2.com>
2023-10-16 20:06:36 +0200

    adding basic form and styling

---
+++ forms/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PersonalInfoForm from './components/PersonalInfoForm';
+
+
+function App() {
+  return (
+    <>
+ <PersonalInfoForm/>
+    </>
+  );
+}
+
+export default App;
---
+++ forms/src/components/PersonalInfoForm.tsx
@@ -0,0 +1,11 @@
+import MainForm from "./formcomponents/FirstNameComponent";
+
+const PersonalForm = () => {
+  return (
+    <>
+      <MainForm />
+    </>
+  );
+};
+
+export default PersonalForm;
